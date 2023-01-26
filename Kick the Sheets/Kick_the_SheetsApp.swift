078918e3e10fd3416d4ea0c7bb5d5a95606f1b78
aking618 <ashsquads@gmail.com>
2023-01-26 15:01:29 -0600

    Initial Commit

---
+++ Kick the Sheets/Kick_the_SheetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kick_the_SheetsApp.swift
+//  Kick the Sheets
+//
+//  Created by Ayren King on 1/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Kick_the_SheetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
