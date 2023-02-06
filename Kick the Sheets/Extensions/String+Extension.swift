06d1804366711a186ac0051ea05472c2cb7a2c93
aking618 <ashsquads@gmail.com>
2023-02-05 22:00:07 -0600

    AK : Fixed layout issues

---
+++ Kick the Sheets/Extensions/String+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  String+Extension.swift
+//  Kick the Sheets
+//
+//  Created by Ayren King on 2/4/23.
+//
+
+import Foundation
+
+extension String {
+    func caseInsensitiveContains(_ otherString: String) -> Bool {
+        let trimmedString = otherString.trimmingCharacters(in: .whitespacesAndNewlines)
+        if trimmedString.isEmpty { return true }
+        
+        return self.lowercased().contains(trimmedString.lowercased())
+    }
+}
--- Kick the Sheets/Extensions/Todo+Extension.swift
+++ Kick the Sheets/Extensions/Todo+Extension.swift
@@ -8,7 +8,15 @@
 import Foundation
 
 extension Todo {
+    static let demoList: [Todo] = Array(repeating: .init(id: 0, dayId: 0, description: "Test Description of item", status: false), count: 3)
+    
     static func completedCount(from list: [Todo]) -> Int {
         list.filter { $0.status }.count
+    }
+    
+    static func filterIndexListByStatus(_ list: [Todo], target: Bool) -> [Int] {
+        return list.enumerated().compactMap { (index, todo) in
+            todo.status == target ? index : nil
+        }
     }
 }
