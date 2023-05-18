7493668caba5efed623afd7c5489c3bfd3f89cf6
aking618 <ashsquads@gmail.com>
2023-05-17 21:07:34 -0500

    AK: Started about popup | swiftformat pre hook

--- Kick the Sheets/Views/Settings/SettingsView.swift
+++ Kick the Sheets/Views/Settings/SettingsView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SettingsView: View {
     @StateObject var viewModel: SettingsViewModel = .init()
-    
+
     @ViewBuilder
     private var header: some View {
         Text("Settings")
@@ -17,7 +17,7 @@ struct SettingsView: View {
             .foregroundColor(KTSColors.textColor.color)
             .padding()
     }
-    
+
     @ViewBuilder
     private var options: some View {
         VStack(spacing: 5) {
@@ -28,7 +28,7 @@ struct SettingsView: View {
             .scrollContentBackground(.hidden)
         }
     }
-    
+
     var body: some View {
         BaseView {
             VStack {
@@ -39,6 +39,17 @@ struct SettingsView: View {
         .foregroundColor(KTSColors.textColor.color)
         .onAppear {
             viewModel.setup()
+        }
+        .popup(isPresented: $viewModel.showAboutPopup) {
+            Text("The popup")
+                .frame(width: 200, height: 60)
+                .background(Color(red: 0.85, green: 0.8, blue: 0.95))
+                .cornerRadius(30.0)
+        } customize: {
+            $0
+                .type(.floater(useSafeAreaInset: true))
+                .position(.top)
+                .closeOnTapOutside(true)
         }
     }
 }
--- Kick the Sheets/Views/Settings/SettingsViewModel.swift
+++ Kick the Sheets/Views/Settings/SettingsViewModel.swift
@@ -9,18 +9,21 @@ import SwiftUI
 
 class SettingsViewModel: ObservableObject {
     @Published var options: [SettingsOption] = []
-    
+    @Published var showAboutPopup: Bool = false
+
     func setup() {
         let resetData = SettingsOption(title: "Reset All Data", image: "trash", style: .destructive, action: handleResetData)
         let about = SettingsOption(title: "About", image: "info.circle", style: .generic, action: handleAbout)
         let darkMode = SettingsOption(title: "Dark Mode", image: "moon", style: .toggle, action: handleDarkMode)
-        
+
         options = [resetData, about, darkMode]
     }
-    
+
     private func handleResetData() {}
-    
-    private func handleAbout() {}
-    
+
+    private func handleAbout() {
+        showAboutPopup = true
+    }
+
     private func handleDarkMode() {}
 }
