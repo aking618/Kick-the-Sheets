8dcde474f22d8e990859541e21f22b2c0d74008d
aking618 <ashsquads@gmail.com>
2023-09-11 21:24:46 -0500

    Fix bash syntax error

--- Kick the SheetsUITests/AppLaunchUITests.swift
+++ Kick the SheetsUITests/AppLaunchUITests.swift
@@ -10,7 +10,6 @@ import XCTest
 final class AppLaunchUITests: XCTestCase {
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
             measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
